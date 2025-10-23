4760dfbdaf5589f474d4013766c2c4ad19035b76
Germán <german.pastor2006@gmail.com>
2025-10-23 12:43:59 +0200

    subida

---
+++ pastormolerogerman/src/main/java/com/salesianostrian/dam/pastormolerogerman/PastormolerogermanApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostrian.dam.pastormolerogerman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastormolerogermanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastormolerogermanApplication.class, args);
+	}
+
+}
---
+++ pastormolerogerman/src/test/java/com/salesianostrian/dam/pastormolerogerman/PastormolerogermanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostrian.dam.pastormolerogerman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastormolerogermanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
